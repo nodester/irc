84f818690c3e28bf3e996c659bb727f10f7d6125
Daniel Bartlett <dan@f-box.org>
2011-02-06 16:50:50 +0000

    Adding socket.io, some html and initial app.js

--- irc.js
+++ irc.js
@@ -1,5 +1,39 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8069);
+var fs = require('fs');
+var io = require('socket.io');
+var express = require('express');
+var ircjs = require('irc-js');
+
+var app = express.createServer();
+
+app.configure(function(){
+  app.use(express.staticProvider(__dirname + '/public'));
+});
+
+app.get('/', function(req, res, next){
+  res.render('./public/index.html');
+});
+app.listen(process.ENV['app_port'], process.ENV['app_host']);
+
+var socket = io.listen(app, {
+  flashPolicyServer: false,
+  transports: ['htmlfile', 'xhr-multipart', 'xhr-polling', 'jsonp-polling']
+});
+
+socket.on('connection', function(client) {
+/*  var my_timer;
+  var my_client = client;
+  my_timer = setInterval(function () {
+    my_client.send(JSON.stringify({ timestamp: (new Date()).getTime() }));
+  }, 1000);
+*/
+  
+  client.on('message', function(data) {
+    console.log(data);
+  });
+
+  client.on('disconnect', function() {
+    clearTimeout(my_timer);
+    console.log('disconnect');
+  });
+});
