feed9e26ccfe227d42d59b386d09e1c581d2dbfd
Paul Maxan <pmaxan@yahoo.com>
2012-03-24 02:39:57 -0400

    Reduced debug messages from socket.io Fixed sorting, added premises for "part" The fix for sorting automatically removed the first join messages. This is to be work upon still. Some code formatting.

--- irc.js
+++ irc.js
@@ -23,13 +23,15 @@ var http    = require('http')
 process.on('uncaughtException', function (err) {
   console.log('Uncaught error: ' + err.stack);
 });
+
 var allowCORS = function(req,res,next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Strict-Transport-Security", "max-age=31556926; includeSubDomains");
   res.header("X-Powered-By","nodeJS");
   next();
-}
+};
+
 app.configure(function(){
   app.use(express.static(__dirname + '/public'));
   app.use(allowCORS);
@@ -46,6 +48,7 @@ console.log('IRC#nodester is running on %d',app.address().port)
 /*
  * Sockets stuff
 */
+io.set('log level', 1); //reduce debug messages
 io.sockets.on('connection', function (client) {
   var socket = client;
   var irc = null;
@@ -66,10 +69,12 @@ io.sockets.on('connection', function (client) {
             realname: nickname + ' via http://irc.nodester.com/'
           }
         });
+        
         console.log(irc)
         irc.connect(function () {
           irc.join(cfg.channel);
         });
+        
         irc.addListener('privmsg', function (message) {
           if (message.params[0] == cfg.channel) {
             client.send(JSON.stringify({
@@ -82,19 +87,20 @@ io.sockets.on('connection', function (client) {
             irc.privmsg(message.person.nick, "I can only talk in #nodester.");
           }
         });
+        
         irc.addListener('join', function (message) {
           if (message.person.nick == nickname) {
             setTimeout(function () {
               irc.names(cfg.channel, function (chan, names) {
-                for(var i in names) {
-                  client.send(JSON.stringify({
-                    messagetype: "join",
-                    from: names[i],
-                    channel: chan
-                  }));
-                }
+                client.send(JSON.stringify({
+                  messagetype: "names",
+                  from: "",
+                  channel: chan,
+                  message: "",
+                  users: names
+                }));
               })
-            }, 1500);
+            }, 3500);
           };
           client.send(JSON.stringify({
             messagetype: "join",
@@ -102,6 +108,7 @@ io.sockets.on('connection', function (client) {
             channel: (message.params[0])
           }));
         });
+        
         irc.addListener('quit', function (message) {
           client.send(JSON.stringify({
             messagetype: "quit",
@@ -112,7 +119,7 @@ io.sockets.on('connection', function (client) {
 
         irc.addListener('error', function () {console.log(arguments)});
       } else {
-        // Maybe handle updaing of nicks one day :)
+        // Maybe handle updating of nicks one day :)
       }
     } else if (obj.hasOwnProperty('messagetype')) {
       switch (obj.messagetype) {
--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
-    var sock      = null;
-    var rv        = null;
-    var nickname  = null;
-    var textInput = $('#text_input');
-    var nick_lis  = {};
-    var logBox    = $('#wrapper');
-    var statusBar = $('#statusBar');
-    var statusMsg = $('#statusmsg');
-    var chatBody  = $('#chat_body');
-    var nick_ul   = $('#nick_ul');
-    var chatForm  = $('#chat-form');
-    var joinForm  = $('#join-form');
+    var sock       = null;
+    var rv         = null;
+    var nickname   = null;
+    var textInput  = $('#text_input');
+    var nicks      = []; //could be an object if later we decide to add the nick attributes (+,... @)
+    var logBox     = $('#wrapper');
+    var statusBar  = $('#statusBar');
+    var statusMsg  = $('#statusmsg');
+    var chatBody   = $('#chat_body');
+    var nick_ul    = $('#nick_ul');
+    var chatForm   = $('#chat-form');
+    var joinForm   = $('#join-form');
     window.counter = 0;
     
     $('#nick').focus();
@@ -125,6 +125,7 @@ $(document).ready(function(){
             message = "<strong>joined the channel</strong>";
             break;
         case "quit":
+        case "part":
             message = "<strong>left the channel</strong>";
             break;
         default:
@@ -140,158 +141,159 @@ $(document).ready(function(){
         scrollBody();
     };
   
-  var addNickToList = function (nick) {
-    if (!nick_lis.hasOwnProperty(nick)) {
-      var li = $('<li value="'+nick+'">'+nick+'<li/>');
-      nick_lis[nick] = li;
-      nick_ul.append(li);
-      sortList(nick_ul);
-    }
-  };
-  
-  var delNickFromList = function (nick) {
-    if (nick_lis.hasOwnProperty(nick)) {
-      nick_ul.removeChild(nick_lis[nick]);
-      delete nick_lis[nick];
-      sortList(nick_ul);
-    }
-  };
-  var handleMessage = function (data) {
-    var obj = JSON.parse(data);
-    window.spinner.stop();
-    statusBar.addClass('off');
-    if (nickname === null) {
-      var tmp = getNickname();
-      if (tmp !== null) {
-        nickname = tmp;
-        rv = sock.send(JSON.stringify({ nickname: nickname }));
-      }
-    } else {
-      if (obj && obj.messagetype) {
-        var s = (obj.from == nickname) ? true : false;
-        switch (obj.messagetype) {
-          case "message":
-            appendMessage(obj.from, obj.message, false);
-            break;
-          case "join":
-            appendEvent(obj.from, obj.messagetype, s);
-            addNickToList(obj.from);
-            break;
-          case "quit":
-            appendEvent(obj.from, obj.messagetype, s);
-            delNickFromList(obj.from);
-            break;
-          default:
-            alert(data);
-            break;
+    var nicksToList = function () {
+        nick_ul.text("");
+        for (var i = 0; i < nicks.length; i++) {
+            var li = $('<li value="'+nicks[i]+'">'+nicks[i]+'<li/>');
+            nick_ul.append(li);
         }
-      } else {
-        console.log(data);
-      }
-    }
-  };
-  var sendMessage = function () {
-    appendMessage(nickname, textInput.val(), true);
-    sock.send(JSON.stringify({
-      messagetype: "message",
-      message: textInput.val()
-    }));
-    textInput.val('');
-  };
-  chatForm.on('submit',function(e){
-	e.preventDefault();
-    if (textInput.val() !== '') {
-      sendMessage();
-    } else {
-      alert('<p> You need to input a name</p>');
-    }
-	$('#text_input').focus();
-	return false;
-  });
+    };
 
-  var sortList = function (oUl) {
-    for(var i in oUl.childNodes) {
-      var x = oUl.childNodes[i];
-      for(var j in oUl.childNodes) {
-        var y = oUl.childNodes[j];
-        if((x.innerText != 'undefined' || y.innerText != 'undefined') && x.innerText > y.innerText) {
-          if(oUl.firstChild != x)
-            oUl.insertBefore(y, x);
+    var handleMessage = function (data) {
+        var obj = JSON.parse(data);
+        window.spinner.stop();
+        statusBar.addClass('off');
+        if (nickname === null) {
+            var tmp = getNickname();
+            if (tmp !== null) {
+                nickname = tmp;
+                rv = sock.send(JSON.stringify({ nickname: nickname }));
+            }
+        };
+        if (obj && obj.messagetype) {
+            var s = (obj.from == nickname) ? true : false;
+            switch (obj.messagetype) {
+                case "message":
+                    appendMessage(obj.from, obj.message, false);
+                    break;
+                case "names":
+                    nicks = [];
+                    for (var i = 0; i < obj.users.length; i++) {
+                        nicks.push(obj.users[i]);
+                    }
+                    nicks.sort(cisort);
+                    nicksToList();
+                    break;
+                case "join":
+                    appendEvent(obj.from, obj.messagetype, s);
+                    nicks.push(obj.from);
+                    nicks.sort(cisort);
+                    nicksToList();
+                    break;
+                case "quit":
+                    appendEvent(obj.from, obj.messagetype, s);
+                    for (var i in nicks) {
+                        if (nicks[i] == obj.from) {
+                            nicks.splice(i,1);
+                          break;
+                        }
+                      }
+                    nicksToList();
+                    break;
+                default:
+                    alert(data);
+                    break;
+            }
+        } else {
+            console.log(data);
         }
-      }
-    }
-  };
-  /* Define innerText for Mozilla based browsers */
-  if((typeof HTMLElement != 'undefined') && (HTMLElement.prototype.__defineGetter__ != 'undefined')) {
-    HTMLElement.prototype.__defineGetter__("innerText", function () {
-      var r = this.ownerDocument.createRange();
-      r.selectNodeContents(this);
-      return r.toString();
+    };
+    
+    var sendMessage = function () {
+        appendMessage(nickname, textInput.val(), true);
+        sock.send(JSON.stringify({
+            messagetype: "message",
+            message: textInput.val()
+        }));
+        textInput.val('');
+    };
+
+    chatForm.on('submit',function(e){
+  	e.preventDefault();
+        if (textInput.val() !== '') {
+            sendMessage();
+        } else {
+            alert('<p> You need to input a name</p>');
+        }
+  	$('#text_input').focus();
+  	return false;
     });
-  }
-  
-  /*
-  var ocolors = {
-    'bold'      : ['\033[1m',  '\033[22m'],
-    'italic'    : ['\033[3m',  '\033[23m'],
-    'underline' : ['\033[4m',  '\033[24m'],
-    'inverse'   : ['\033[7m',  '\033[27m'],
-    'white'     : ['\033[37m', '\033[39m'],
-    'grey'      : ['\033[90m', '\033[39m'],
-    'black'     : ['\033[30m', '\033[39m'],
-    'blue'      : ['\033[34m', '\033[39m'],
-    'cyan'      : ['\033[36m', '\033[39m'],
-    'green'     : ['\033[32m', '\033[39m'],
-    'magenta'   : ['\033[35m', '\033[39m'],
-    'red'       : ['\033[31m', '\033[39m'],
-    'yellow'    : ['\033[33m', '\033[39m']
-  };*/
-  var colors = {
-     'p':['<p>','</p>'],
-     '[1m'  :['<strong>','</strong>'],
-     '[22m'  :['<strong>','</strong>'],
-     '[3m'  :['<i>','</i>'],
-     '[23m'  :['<i>','</i>'],
-     '[4m'  :['<u>','</u>'],
-     '[24m'  :['<u>','</u>'],
-     '[7m'  :['<span>','</span>'],
-     '[27m'  :['<span>','</span>'],
-     '[37m' :['<span style="color:white">','</span>'],
-     '[90m' :['<span style="color:grey">','</span>'],
-     '[30m' :['<span style="color:#444">','</span>'],
-     '[34m' :['<span style="color:blue">','</span>'],
-     '[36m' :['<span style="color:cyan">','</span>'],
-     '[32m' :['<span style="color:green">','</span>'],
-     '[35m' :['<span style="color:magenta">','</span>'],
-     '[31m' :['<span style="color:red">','</span>'],
-     '[33m' :['<span style="color:yellow">','</span>']
-  };
 
-  var giveMeColors = function(str) {
-    var old   = str = str || '[44m'+str+'[43m';
-        str   = str.split(str.search(/\[[0-9][0-9]m/));
-    var text  = str.join('').split(/\[[0-9][0-9]m/g);
-    var color = str.join('').match(/\[[0-9][0-9]m|\[[0-9]m/g)||'';
-    var loop  = -1;
-    var dohs  = 0;
-    while (color[loop + 1]){
-      var prev = ++loop;
-      var next = ++loop;
-      if (color[prev]){
-        if (colors[color[prev]]){
-          var math = (old.search('\\'+color[next])-old.search('\\'+color[prev]))
-          if (!(math > 0 && math < 5)){
-            old = old.replace(new RegExp('\\'+color[prev]), colors[color[prev]][0])
-            old = old.replace(new RegExp('\\'+color[next]),colors[color[prev]][1]);
-          } else {
-            old = old.replace(new RegExp('\\'+color[prev]), colors[color[prev]][0])
-            old = old.replace(new RegExp('\\'+color[next]), colors[color[next]][0])
-            old = old.replace(new RegExp('\\'+color[++loop]), colors[color[next]][1])
-            old = old.replace(new RegExp('\\'+color[++loop]),colors[color[prev]][1]);
-          }
+/*  var ocolors = {
+      'bold'      : ['\033[1m',  '\033[22m'],
+      'italic'    : ['\033[3m',  '\033[23m'],
+      'underline' : ['\033[4m',  '\033[24m'],
+      'inverse'   : ['\033[7m',  '\033[27m'],
+      'white'     : ['\033[37m', '\033[39m'],
+      'grey'      : ['\033[90m', '\033[39m'],
+      'black'     : ['\033[30m', '\033[39m'],
+      'blue'      : ['\033[34m', '\033[39m'],
+      'cyan'      : ['\033[36m', '\033[39m'],
+      'green'     : ['\033[32m', '\033[39m'],
+      'magenta'   : ['\033[35m', '\033[39m'],
+      'red'       : ['\033[31m', '\033[39m'],
+      'yellow'    : ['\033[33m', '\033[39m']
+    };
+*/
+    var colors = {
+       'p'    :['<p>','</p>'],
+       '[1m'  :['<strong>','</strong>'],
+       '[22m' :['<strong>','</strong>'],
+       '[3m'  :['<i>','</i>'],
+       '[23m' :['<i>','</i>'],
+       '[4m'  :['<u>','</u>'],
+       '[24m' :['<u>','</u>'],
+       '[7m'  :['<span>','</span>'],
+       '[27m' :['<span>','</span>'],
+       '[37m' :['<span style="color:white">','</span>'],
+       '[90m' :['<span style="color:grey">','</span>'],
+       '[30m' :['<span style="color:#444">','</span>'],
+       '[34m' :['<span style="color:blue">','</span>'],
+       '[36m' :['<span style="color:cyan">','</span>'],
+       '[32m' :['<span style="color:green">','</span>'],
+       '[35m' :['<span style="color:magenta">','</span>'],
+       '[31m' :['<span style="color:red">','</span>'],
+       '[33m' :['<span style="color:yellow">','</span>']
+    };
+
+    var giveMeColors = function(str) {
+        var old = str = str || '[44m'+str+'[43m';
+        str = str.split(str.search(/\[[0-9][0-9]m/));
+        var text  = str.join('').split(/\[[0-9][0-9]m/g);
+        var color = str.join('').match(/\[[0-9][0-9]m|\[[0-9]m/g)||'';
+        var loop  = -1;
+        var dohs  = 0;
+        while (color[loop + 1]){
+            var prev = ++loop;
+            var next = ++loop;
+            if (color[prev]){
+                if (colors[color[prev]]){
+                    var math = (old.search('\\'+color[next])-old.search('\\'+color[prev]))
+                    if (!(math > 0 && math < 5)){
+                        old = old.replace(new RegExp('\\'+color[prev]), colors[color[prev]][0])
+                        old = old.replace(new RegExp('\\'+color[next]),colors[color[prev]][1]);
+                    } else {
+                        old = old.replace(new RegExp('\\'+color[prev]), colors[color[prev]][0])
+                        old = old.replace(new RegExp('\\'+color[next]), colors[color[next]][0])
+                        old = old.replace(new RegExp('\\'+color[++loop]), colors[color[next]][1])
+                        old = old.replace(new RegExp('\\'+color[++loop]),colors[color[prev]][1]);
+                    }
+                }
+            }
         }
-      }
-    }
-    return old.replace(/\[[0-9]m|\[|[0-9][0-9]m|/g,'');
-  }
+        return old.replace(/\[[0-9]m|\[|[0-9][0-9]m|/g,'');
+    };
+    
+    //case insensitive compare
+    var cisort = function(x, y){ 
+        var a = x.toUpperCase(); 
+        var b = y.toUpperCase(); 
+        if (a > b) {
+            return 1; 
+        } else if (a < b) {
+            return -1;
+        } else {
+            return 0; 
+        }
+    }; 
 });
