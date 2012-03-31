eb70d87faa41937881f2a4b77056b6b92dd6cb73
Paul Maxan <pmaxan@yahoo.com>
2012-03-31 13:41:48 -0400

    Added comments to the code. Commented out the logging of irc parameters.

--- irc.js
+++ irc.js
@@ -23,7 +23,6 @@ var http    = require('http')
   , app     = express.createServer()
   , io      = require('socket.io').listen(app);
 
-
 //when the app started
 var starttime = (new Date()).getTime();
 //get usage RAM in bytes
@@ -95,11 +94,23 @@ io.sockets.on('connection', function (client) {
           }
         });
         
-        console.log(irc)
+        //console.log(irc)
+
+        /*
+         * Initiating a connection to the irc server
+         * When the connection with the irc server is established and ready,
+         * we issue a call to join.
+         */
         irc.connect(function () {
           irc.join(cfg.channel);
         });
         
+        /*
+         * Handler for private messages
+         * There is no private messaging using the web client.
+         * in case o private message is received, a message is sent back to the caller
+         * explaining that there is no privacy!
+         */
         irc.addListener('privmsg', function (message) {
           if (message.params[0] == cfg.channel) {
             client.send(JSON.stringify({
@@ -109,10 +120,14 @@ io.sockets.on('connection', function (client) {
               message: (message.params[1])
             }));
           } else {
-            irc.privmsg(message.person.nick, "I can only talk in #nodester.");
+            irc.privmsg(message.person.nick,
+              "Automatic: I am using a web client. I can only talk on channel #nodester.");
           }
         });
         
+        /*
+         * Handler for join
+         */
         irc.addListener('join', function (message) {
           client.send(JSON.stringify({
             messagetype: "join",
@@ -121,7 +136,9 @@ io.sockets.on('connection', function (client) {
           }));
         });
         
-        //topic 332
+        /*
+         * Handler for the topic, 332
+         */
         irc.addListener('332', function (raw) {
           client.send(JSON.stringify({
             messagetype: "topic",
@@ -134,7 +151,10 @@ io.sockets.on('connection', function (client) {
           }));
         });
         
-        //names list incoming 353
+        /*
+         * Handler for names.
+         * There can be multiple such calls as the names are retrieved, 353
+         */
         irc.addListener('353', function (raw) {
           client.send(JSON.stringify({
             messagetype: "names",
@@ -148,7 +168,9 @@ io.sockets.on('connection', function (client) {
           }));
         });
         
-        //end of names list 366
+        /*
+         * Handler for end of names list, 366
+         */
         irc.addListener('366', function (raw) {
           client.send(JSON.stringify({
             messagetype: "endnames",
@@ -159,7 +181,10 @@ io.sockets.on('connection', function (client) {
           }));
         });
 
-        //on disconnect from irc server
+        /*
+         * Handler for quitting
+         * This event will not be triggered after an irc.quit() call
+         */
         irc.addListener('quit', function (message) {
           client.send(JSON.stringify({
             messagetype: "quit",
@@ -168,20 +193,9 @@ io.sockets.on('connection', function (client) {
           }));
         });
 
-        //on nickname already in use, 433
-        irc.addListener('433', function (message) {
-          client.send(JSON.stringify({
-            messagetype: "433",
-            //rejected nick
-            from: (message.params[1]),
-            //the irc server as channel
-            channel: message.server,
-            //the rejection message, usually "Nickname is already in use."
-            message: (message.params[2])
-          }));
-        });
-
-        //on parting the channel but remaining connected to the irc server
+        /*
+         * Handler for parting the channel, while remaining connected to the irc server
+         */
         irc.addListener('part', function (message) {
           client.send(JSON.stringify({
             messagetype: "part",
@@ -190,7 +204,11 @@ io.sockets.on('connection', function (client) {
           }));
         });
 
-        //motd
+        /*
+         * Handler for a motd line, 372
+         * There are multiple calls as such until the entire motd is received
+         * Note: not all servers emit a motd!
+         */
         irc.addListener('372', function (raw) {
           client.send(JSON.stringify({
             messagetype: "motd",
@@ -203,6 +221,9 @@ io.sockets.on('connection', function (client) {
           }));
         });
 
+        /*
+         * Handler for the end of motd, 376
+         */
         irc.addListener('376', function (raw) {
           client.send(JSON.stringify({
             messagetype: "endmotd",
@@ -211,10 +232,15 @@ io.sockets.on('connection', function (client) {
           }));
         });
         
-        //welcome from ircserver
+        /*
+         * Handler for the welcome message
+         * This indicates that the irc server accepted our request, and while there
+         * are further possible notifications, for us this is a sign that we can
+         * close the login screen and open the main window.
+         */
         irc.addListener('001', function (raw) {
           webUsers.push(irc.options.nick);
-          bWebUsersDirty = true;
+          bWebUsersDirty = true; //for self
           client.send(JSON.stringify({
             messagetype: "001",
             //server
@@ -226,6 +252,9 @@ io.sockets.on('connection', function (client) {
           }));
         });
         
+        /*
+         * Handler for notices
+         */
         irc.addListener('notice', function (message) {
           if (message.person !== undefined) {
             //notice for content
@@ -246,7 +275,27 @@ io.sockets.on('connection', function (client) {
           }
         });
         
-        //all IRC errors but 433 handles above
+        /*
+         * Handler for irc error 433: nick already in use.
+         * We use this particular handler for the login screen
+         */
+        irc.addListener('433', function (message) {
+          client.send(JSON.stringify({
+            messagetype: "433",
+            //rejected nick
+            from: (message.params[1]),
+            //the irc server as channel
+            channel: message.server,
+            //the rejection message, usually "Nickname is already in use."
+            message: (message.params[2])
+          }));
+        });
+
+        /*
+         * Handler for all irc server errors
+         * This has nothing to do with the irc-js implementation errors
+         * Handles all error messages but 433 handled above
+         */
         for (var err = 400; err < 600; err++) {
           if (err != 433) {
             irc.addListener(err, function (raw) {
@@ -260,7 +309,10 @@ io.sockets.on('connection', function (client) {
           }; //if
         }; //for
 
-        //all client errors (e.g., timeouts)
+        /*
+         * Handler for our own client
+         * e.g., timeout
+         */
         irc.addListener('error', function () {
           client.send(JSON.stringify({ 
             messagetype: "error"
@@ -270,7 +322,10 @@ io.sockets.on('connection', function (client) {
         // Maybe handle updating of nicks one day :)
       }
     } else if (obj.hasOwnProperty('messagetype')) {
-      switch (obj.messagetype) {
+      /*
+       * Handler for a web client request to send a message to the irc server
+       */
+        switch (obj.messagetype) {
         case "message":
           irc.privmsg(cfg.channel, (obj.message));
           break;
@@ -279,7 +334,11 @@ io.sockets.on('connection', function (client) {
           break;
       }
     } else if (obj.hasOwnProperty('statistics')) {
-        //there is a lag here, at maximum though it is 16 seconds since last update
+        /*
+         * Handler for a web client request for statistics
+         * The statistics are gathered every 15 seconds. There will be a lag
+         * of less than 15 seconds since the last statistics "crop"
+         */
         client.send(JSON.stringify({
           messagetype: "statistics",
           st: starttime,
@@ -289,6 +348,11 @@ io.sockets.on('connection', function (client) {
           wud: bWebUsersDirty
         }));
     } else if (obj.hasOwnProperty('webusers')) {
+        /*
+         * Handler for a webclient request for webusers
+         * These are the users that connect to the irc server through
+         * our irc client. There is no mode attributes attached to these users.
+         */
         bWebUsersDirty = false;
         client.send(JSON.stringify({
           messagetype: "webusers",
@@ -297,6 +361,12 @@ io.sockets.on('connection', function (client) {
     };
   });
 
+  /*
+   * Handler for socket disconnect
+   * This event indicates that the connection in between the web client and our
+   * irc client (server for the socket communication to the web client) has been
+   * lost. Currently we close the irc connection for that particular web client.
+   */
   client.on('disconnect', function() {
     if (irc){
       for (var i = 0; i < webUsers.length; i++) {
